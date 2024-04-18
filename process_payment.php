56972b6c75084133d8480474dd930e3a62ebdc41
manolo0006 <167484710+manolo0006@users.noreply.github.com>
2024-04-19 01:19:39 +0200

    Update process_payment.php

--- process_payment.php
+++ process_payment.php
@@ -1,1 +1,25 @@
+<?php
+// Controlla se le credenziali dell'amministratore sono valide (questo è solo un esempio, dovresti utilizzare un metodo sicuro per autenticare gli amministratori)
+$admin_username = "admin";
+$admin_password = "password";
+
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if ($username === $admin_username && $password === $admin_password) {
+        // Credenziali corrette, reindirizza l'amministratore alla pagina riservata agli amministratori
+        header("Location: pagina_amministratori.php");
+        exit;
+    } else {
+        // Credenziali non corrette, reindirizza altrove (ad esempio, pagina di errore)
+        header("Location: pagina_errore.php");
+        exit;
+    }
+} else {
+    // Se non sono stati inviati username e password, reindirizza altrove
+    header("Location: pagina_errore.php");
+    exit;
+}
+?>
 
